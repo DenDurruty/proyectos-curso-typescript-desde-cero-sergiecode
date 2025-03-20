aa6b8698ad513d59f7e63cace5ea57024a23c1d7
Den Durruty <dendurruty@users.noreply.github.com>
2025-03-20 18:15:47 -0300

    Ejemplos de tipado de datos primitivos

---
+++ script3.js
@@ -0,0 +1,47 @@
+"use strict";
+// BOOLEAN
+/*let estudiasteJavascript: boolean = false
+
+if(estudiasteJavascript){
+    console.log('Puedes seguir viendo este curso de Typescript')
+}else{
+    console.log('Primero tenés que ver el curso de Javasscript')
+}
+*/
+// NUMBER
+/*let interMiami: number = 11
+let fcDallas: number = 11
+let messi: number = 1
+let juegaMessi: boolean = true
+
+function jugar(equipo1: number, equipo2: number, juegaMessi: boolean): void {
+    let motivo: string = ''
+    if(juegaMessi) {
+        equipo1 += messi
+        motivo = ' porque juega Messi'
+    }
+    if(equipo1 > equipo2) console.log (`Gana Inter Miami${motivo}.`)
+    if(equipo1 == equipo2) console.log (`Empatan.`)
+    if(equipo1 < equipo2) console.log (`Gana FC Dallas.`)
+}
+
+jugar(interMiami, fcDallas, juegaMessi)
+*/
+// ANY
+// TODO: cambiar any por el tipado correspondiente
+/*let disney:any;
+
+disney = 'Star Wars y Marvel'
+console.log(disney)
+
+disney = 1500000
+console.log(disney)
+
+disney = true
+console.log(disney)
+*/
+//ARRAY
+//let arregloNumeros: any[] = [1, "Ricardo", 3, 4, 5, 6]
+let arregloNumeros = [1, 2, 3, 4, 5, 6];
+let arregloTexto = ['HTML', 'CSS', 'JS'];
+//arregloTexto[0].indexOf()
